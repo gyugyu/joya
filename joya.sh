e41175204eb75010e91c22723344aabc2c8afc35
Yusuke Yagyu <gyugyu@pepabo.com>
2019-12-26 12:03:38 +0900

    DATE -> TIMES

--- joya.sh
+++ joya.sh
@@ -4,11 +4,11 @@ set -eux
 
 YEAR=`date +%Y`
 START=`date -j -f "%Y/%m/%d %H:%M:%S" "${YEAR}/12/31 22:13:00" +%s`
-DAYS=0
+TIMES=0
 
-while [ $DAYS -lt 108 ]; do
-  TARGET=`date -r ${START} -v +${DAYS}M +%s`
+while [ $TIMES -lt 108 ]; do
+  TARGET=`date -r ${START} -v +${TIMES}M +%s`
   LANG=en_US COMMIT=`date -r ${TARGET} +"%a %b %d %H:%M:00 %Y +0900"`
   git commit --allow-empty --message "${COMMIT}" --date="${COMMIT}"
-  DAYS=$((DAYS + 1))
+  TIMES=$((TIMES + 1))
 done
